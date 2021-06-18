a4b471b6695a1ea9d237229b5ab6d324bf0e5f3d
Arkadii Vodolazskyi <jswift168@gmail.com>
2021-06-18 09:18:26 +0300

    Using Forms in Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,35 @@
 <template>
-  <h1>Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Start</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <Results v-show="score" :score="score" />
+  <SignupForm />
 </template>
 
 <script>
-import Block from './components/Block.vue';
-import Results from './components/Results.vue';
+import SignupForm from "./components/SignupForm.vue";
 
 export default {
   name: "App",
   components: {
-    Block,
-    Results
+    SignupForm
   },
   data() {
     return {
-      isPlaying: false,
-      delay: null,
-      score: null
     };
   },
   methods: {
-    start() {
-      this.score = null;
-      this.isPlaying = true;
-      this.delay = 2000 + Math.round(Math.random() * 5000);
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
-    }
   }
 };
 </script>
 
-<style lang="sass">
-body
-  margin: 0
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
 </style>
