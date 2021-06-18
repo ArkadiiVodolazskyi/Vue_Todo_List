c8584bdf1841f7c6318a163d63a66fd98d3e6c49
Arkadii Vodolazskyi <jswift168@gmail.com>
2021-06-18 12:03:45 +0300

    Fetching data, JSON Server

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <SignupForm />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
+  </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
+  <router-view/>
 </template>
 
 <script>
-import SignupForm from "./components/SignupForm.vue";
-
 export default {
-  name: "App",
-  components: {
-    SignupForm
-  },
-  data() {
-    return {
-    };
-  },
   methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    }
   }
-};
+}
 </script>
 
 <style>
@@ -26,10 +35,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 960px;
+  margin: 0 auto;
 }
-body {
-  margin: 0;
-  background: #eee;
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+#nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
